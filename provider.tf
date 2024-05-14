4bf13956aecb8438f422ee34328bd4273c41610b
Prathyusha12m <prathyu12m@gmail.com>
2024-05-15 00:34:10 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,4 +0,0 @@
-provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
-}
