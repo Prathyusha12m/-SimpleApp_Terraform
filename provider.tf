7cbf3f39c751076df767614a55366f6e7095015a
Prathyusha12m <prathyu12m@gmail.com>
2024-05-14 22:16:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,8 +2,3 @@ provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
-
-provider "azurerm" {
-  storage_use_azuread = true
-  features {}
-}
