93b10dcae360ab06c8b17a9487e0406d722ebb23
Prathyusha12m <prathyu12m@gmail.com>
2024-05-15 00:33:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,16 @@ terraform {
       version = "3.0.2"
     }
   }
+  required_version = ">= 1.1.0"
+  backend "azurerm" {}
 }
+provider "azurerm" {
+   feautures {}
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "fitnessgeek-rg"
-  location = var.location-rg
+  name     = "Fitnessgeek-rg"
+  location =  var.location-rg
   tags = {
     "Application" = "DemoApp"
   }
