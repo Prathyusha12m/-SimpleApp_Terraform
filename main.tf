0191d7c44655325b039814b8982425aea167b3c2
Prathyusha12m <prathyu12m@gmail.com>
2024-05-15 13:42:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
-resource "azurerm_resource_group" "default" {
+resource "azurerm_resource_group" "rg" {
   name     =  var.name-rg
   location =  var.location-rg
   tags = {
